c99bf218ba3e41437866f8404438679908f14fe7
REL <arielbenzikri@gmail.com>
2022-11-06 21:15:45 +0200

    starting project, first commit

---
+++ MainApp/main.py
@@ -0,0 +1,9 @@
+# here is the list of all the abs workout functions
+
+def ab_wheel_rollout():
+    ab_wheel = False
+    workout = False
+
+    x = input("Starting exercise? (yes/no)\n")
+    y = input("how many reps to do?\n")
+    z = input("how many sets to do?\n")
